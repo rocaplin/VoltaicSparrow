25e59d57976e01c79ba51f617fe63325f0cab63b
Scott Cornaby <scott.cornaby@ufl.edu>
2023-02-02 19:23:25 -0600

    Proof of concept for connecting the custom action server and mongoDB

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -10,6 +10,7 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
+from pymongo import MongoClient
 
 class ActionHelloWorld(Action):
 
@@ -20,6 +21,21 @@ class ActionHelloWorld(Action):
              tracker: Tracker,
              domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-         dispatcher.utter_message(text="Hello From The Action Server!")
+         client = MongoClient('mongo-db',27017)
+         db = client.test_database
+         counters = db.counters
+         
+         count = counters.find_one({"topic": "hello"})
+         
+         if not count:
+            count = {"topic": "hello",
+                            "count": 0}
+        
+         count["count"] = count["count"] + 1
+        
+         counters.replace_one({"topic":"hello"},count, True)
+            
+
+         dispatcher.utter_message(text="Hello From The Action Server! You have said 'hello' to the DB " + str(count["count"]) + " time(s)!")
 
          return []
