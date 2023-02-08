01876162be940bb085927daa58a441184ba4a4d5
Scott Cornaby <scott.cornaby@ufl.edu>
2023-02-07 18:19:48 -0600

    New version of the UI server that runs react

--- ui/app.js
+++ ui/app.js
@@ -1,41 +1,27 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import React from 'react';
+import './App.css';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+class App extends React.Component{
 
-var app = express();
+	render() {
+	  return(
+	   
+		  <div>
+			<h1>Welcome To Chompsci React Version!</h1>
+			<div id="rasa-chat-widget" data-websocket-url="http://localhost:5005"></div>
+		  </div>
+	  )
+	}
+	
+	componentDidMount () {
+		const script = document.createElement("script");
+		script.src = "/javascripts/rasa-widget.js";
+		script.async = true;
+		document.body.appendChild(script);
+	}
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+}
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+export default App;
