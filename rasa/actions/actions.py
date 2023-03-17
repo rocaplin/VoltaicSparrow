47a739b3936a2a1142ffe8e93d1a2451f47c717d
Christopher Schmidt <schmidtc1@ufl.edu>
2023-03-17 11:53:15 -0400

    Additional test stories, adjust NLU, add buttons

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -39,7 +39,6 @@ class ActionHelloWorld(Action):
             
 
          dispatcher.utter_message(text="Hello From The Action Server! You have said 'hello' to the DB " + str(count["count"]) + " time(s)!")
-
          return []
 
 class ActionRequestTopics(Action):
@@ -74,14 +73,61 @@ class ActionRequestTopics(Action):
                 dispatcher.utter_message(text="I'm not well-trained on that topic...")
             else:
                 str = ""
+                str += topic.lower()
+                dispatcher.utter_message(text=("I'm not well-trained on " + str))
+        else:
+            if not requested_topic["related_courses"]:
+                dispatcher.utter_message(text=("Here is what I know about " + topic.lower() + ": \n" + requested_topic["description"]))
+                dispatcher.utter_message(text=("It doesn't seem like there are any classes on this..."))
+            else:
+                dispatcher.utter_message(text=("Here is what I know about " + topic.lower() + ": \n" + requested_topic["description"]))
+                dispatcher.utter_message(text=("What would you like to know about this topic?"), buttons=[
+                    {"payload": "/request_class_by_topic{\"topic\": \"" + topic.lower() + "\"}", "title": "Classes?"},
+                    {"payload": "/deny", "title": "Courses?"},
+                    ])
+                
+
+        return []
+    
+
+class ActionRequestByTopic(Action):
+
+    def name(self) -> Text:
+        return "action_request_class_by_topic"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        client = MongoClient('mongo-db',27017)
+
+        db = client["chompsci"]
+        topics = db.topics
+
+        topic = None
+
+        for entity in tracker.latest_message['entities']:
+            if entity["entity"] == 'topic':
+                topic = entity['value']
+        
+
+        # requested_topic = None
+        # if topic in self.data:
+            
+        requested_topic = topics.find_one({"lower_topic": topic.lower()})
+            #https://stackoverflow.com/questions/6266555/querying-mongodb-via-pymongo-in-case-insensitive-efficiently
+
+        if not requested_topic:
+            if not topic:
+                dispatcher.utter_message(text="I'm not well-trained on that topic...")
+            else:
+                str = ""
                 str += topic
                 dispatcher.utter_message(text=("I'm not well-trained on " + str))
         else:
             if not requested_topic["related_courses"]:
-                dispatcher.utter_message(text=("Here is what I know about " + topic + ": \n" + requested_topic["description"]))
                 dispatcher.utter_message(text=("It doesn't seem like there are any classes on this..."))
             else:
-                dispatcher.utter_message(text=("Here is what I know about " + topic + ": \n" + requested_topic["description"]))
-                dispatcher.utter_message(text=("You can learn more about it in these classes: " + requested_topic["related_courses"]))
+                dispatcher.utter_message(text=("You can learn more about " + topic.lower() + " in these classes: " + requested_topic["related_courses"]))
 
         return []
